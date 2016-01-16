c00649bd2a94c3c2627130a9b9850de9e25b41ba
karoltyminski <karoltyminski@ktyminski>
2016-01-16 22:55:48 +0100

    update plikow pom i dodanie domainow dla 2 tabel

---
+++ mieszkaniabaza/src/main/java/com/mieszkaniabaza/domain/Mieszkania.java
@@ -0,0 +1,83 @@
+package com.mieszkaniabaza.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+
+@Entity
+@NamedQueries({ 
+	@NamedQuery(name = "Mieszkania.wszystkie", query = "Select m from Mieszkanie m")
+})
+public class Mieszkania {
+
+	private Long idMieszkania;
+ 	private Wynajmujacy wynajmujacy;
+	private String ulica="";
+	private Double cena = 0.0;
+	private String opis = "";
+
+	 public Mieszkania() {
+	}
+
+	public Mieszkania(Wynajmujacy wynajmujacy, String ulica, Double cena, String opis)
+	{
+	this.wynajmujacy = wynajmujacy;
+	this.ulica = ulica;
+	this.cena = cena;
+	this.opis = opis;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return idMieszkania;
+	}
+	public void setId(Long idMieszkania) {
+		this.idMieszkania = idMieszkania;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "id_Wynajmujacy", nullable = false)
+	public Wynajmujacy getTrumna() {
+	return wynajmujacy;
+	}
+	public void setTrumna(Wynajmujacy wynajmujacy) { this.wynajmujacy = wynajmujacy; }
+	
+	
+
+	public String getUlica() {
+		return ulica;
+	}
+
+	public void setUlica(String ulica) {
+		this.ulica = ulica;
+	}
+
+	@Min(0)
+	public Double getCena() {
+		return cena;
+	}
+	public void setCena(Double cena) {
+		this.cena = cena;
+	}
+
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+}
+
+
---
+++ mieszkaniabaza/src/main/java/com/mieszkaniabaza/domain/Wynajmujacy.java
@@ -0,0 +1,111 @@
+package com.mieszkaniabaza.domain;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@NamedQueries({ 
+	@NamedQuery(name = "Wynajmujacy.all", query = "Select w from Wynajmujacy w")
+})
+public class Wynajmujacy {
+	
+	private Long idWynajmujacy;
+	private String imie;
+	private String nazwisko;// cena = 0.0;
+	private Long pesel;// ilosc = 0;
+
+	private List<Mieszkania> mieszkania = new ArrayList<Mieszkania>();
+	
+	@OneToMany(mappedBy = "Wynajmujacy", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JsonIgnore
+	public List<Mieszkania> getMieszkania() {
+	return mieszkania;
+	}
+	public void setPogrzeby(List<Mieszkania> mieszkania) {
+	this.mieszkania = mieszkania;
+	}
+
+
+
+	public Long getIdWynajmujacy() {
+		return idWynajmujacy;
+	}
+
+	public void setIdWynajmujacy(Long idWynajmujacy) {
+		this.idWynajmujacy = idWynajmujacy;
+	}
+
+	public String getImie() {
+		return imie;
+	}
+
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+
+	public Wynajmujacy(Long idWynajmujacy, String imie, String nazwisko, Long pesel, List<Mieszkania> mieszkania) {
+		super();
+		this.idWynajmujacy = idWynajmujacy;
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.pesel = pesel;
+		this.mieszkania = mieszkania;
+	}
+
+	public String getNazwisko() {
+		return nazwisko;
+	}
+
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+
+	public Long getPesel() {
+		return pesel;
+	}
+
+	public void setPesel(Long pesel) {
+		this.pesel = pesel;
+	}
+
+	public void setMieszkania(List<Mieszkania> mieszkania) {
+		this.mieszkania = mieszkania;
+	}
+
+	public Wynajmujacy() {
+	}
+
+	public Wynajmujacy(String imie, String nazwisko, Long pesel) {
+	this.imie = imie;
+	this.nazwisko = nazwisko;
+	this.pesel = pesel;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return idWynajmujacy;
+	}
+	public void setId(Long id) {
+		this.idWynajmujacy = id;
+	}
+
+
+	
+
+}
